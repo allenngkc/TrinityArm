63dda260a96d6c64788cadbdc5515a6697ba061d
allenng321 <pikachuexeallen@gmail.com>
2023-09-17 04:48:09 -0400

    Data parsing from Adhawk

--- sketch_sep17a/sketch_sep17a.ino
+++ sketch_sep17a/sketch_sep17a.ino
@@ -1,6 +1,5 @@
 #include <Servo.h>
-import serial
-arduinoData=serial.Serial('com14', 115200)
+
 Servo myservo1;  // create servo object to control a servo
 // twelve servo objects can be created on most boards
 Servo myservo2;
@@ -15,10 +14,13 @@ float xpos;
 float ypos;
 int angle1=0;
 int angle2=0;
+String cmd;
 void setup() {
   myservo1.attach(8);  // attaches the servo on pin 9 to the servo object
   myservo2.attach(9);
   Serial.begin(9600);
+
+  Serial.begin(115200);
 }
 // void loop() {
 //   for (pos = 0; pos <= 180; pos += 1) { // goes from 0 degrees to 180 degrees
@@ -29,34 +31,111 @@ void setup() {
 //   }
 // }
 void loop() {
-  xpos = random(50); //receive xpos from glasses
-  ypos = random(30); // receive ypos from glasses
-  q2 = acos((pow(double(xpos), 2.0) + pow(double(ypos),2.0) -pow(double(a1),2) -pow(double(a2),2.0)/(2*a1*a2)));
-  q1 = atan(ypos/xpos) - atan((a2*sin(q2))/(a1+a2*cos(q2)));
-  move1 = int((q1 - angle1)*(360/2*3.141592654));
-  move2 = int((q1 + q2 - angle2)*(360/2*3.141592654));
-  if (move1 >= move2) {
-    int pos2 = angle2;
-    for (pos = angle1; pos <= move1+angle1; pos+=1) {
-      myservo1.write(pos);
-      myservo2.write(pos2);
-      if (pos2 < move2+angle2) {
-        pos2 += 1;
-      delay(5);
+  while(Serial.available()==0){
+    String msg = Serial.readStringUntil('\r');
+    Serial.println(msg);
+  }
+  cmd = Serial.readStringUntil('\r');
+  Serial.println(cmd);
+
+  servoarm("left");
+
+}
+
+void servoarm(direction) {
+  Servo motorarm;
+  int pos=0;    // variable to store the servo position
+  int move1;
+  int move2;
+  void setup() {
+    motorarm.attach(9);  // attaches the servo on pin 9 to the servo object
+    Serial.begin(9600);
+  }
+  void loop(direction) {
+    if (direction == "left") {
+      move1 = 20;
+      for (pos = 0; pos <= move1; pos+=1) {
+        if (pos < move1) {
+          pos += 1;
+        }
+        delay(5);
+        motorarm.write(pos);
+      } //receive xpos from glasses
+    }
+    else if (direction == "right") {
+      move1 = -20;
+      for (pos = 0; pos >= move1; pos-=1) {
+        if (pos > move1) {
+          pos -= 1;
+        }
+        delay(5);
+        motorarm.write(pos);
       }
     }
-  } else {
-    int pos1 = angle1;
-    Serial.println("Pos 2: " + myservo2.read());
-    Serial.println("Pos 1: " + myservo1.read());
-    for (pos = angle2; pos <= move2+angle2; pos+=1) {
-      myservo2.write(pos);
-      myservo1.write(pos1);
-      if (pos1 < move1+angle1) {
-        pos1 += 1;
-      delay(5);
+  }
+}
+
+void servowrist(direction) {
+  Servo motorwrist;
+  int pos=0;    // variable to store the servo position
+  int move1;
+  int move2;
+  void setup() {
+    motorwrist.attach(8);  // attaches the servo on pin 9 to the servo object
+    Serial.begin(9600);
+  }
+void loop(direction) {
+  if (direction == "up") {
+    move1 = 90;
+    for (pos = 0; pos <= move1; pos+=1) {
+      if (pos < move1) {
+        pos += 1;
       }
+      delay(5);
+      motorwrist.write(pos);
     }
+  }
+  else if (direction == "down") {
+    //receive xpos from glasses
+    move1 = -90;
+    for (pos = 0; pos >= move1; pos-=1) {
+      if (pos < move1) {
+        pos -= 1;
+      }
+      delay(5);
+      motorwrist.write(pos);
+    }
+  }
+}
+
+  // xpos = random(50); //receive xpos from glasses
+  // ypos = random(30); // receive ypos from glasses
+  // q2 = acos((pow(double(xpos), 2.0) + pow(double(ypos),2.0) -pow(double(a1),2) -pow(double(a2),2.0)/(2*a1*a2)));
+  // q1 = atan(ypos/xpos) - atan((a2*sin(q2))/(a1+a2*cos(q2)));
+  // move1 = int((q1 - angle1)*(360/2*3.141592654));
+  // move2 = int((q1 + q2 - angle2)*(360/2*3.141592654));
+  // if (move1 >= move2) {
+  //   int pos2 = angle2;
+  //   for (pos = angle1; pos <= move1+angle1; pos+=1) {
+  //     myservo1.write(pos);
+  //     myservo2.write(pos2);
+  //     if (pos2 < move2+angle2) {
+  //       pos2 += 1;
+  //     delay(5);
+  //     }
+  //   }
+  // } else {
+  //   int pos1 = angle1;
+  //   Serial.println("Pos 2: " + myservo2.read());
+  //   Serial.println("Pos 1: " + myservo1.read());
+  //   for (pos = angle2; pos <= move2+angle2; pos+=1) {
+  //     myservo2.write(pos);
+  //     myservo1.write(pos1);
+  //     if (pos1 < move1+angle1) {
+  //       pos1 += 1;
+  //     delay(5);
+  //     }
+  //   }
 
   //   if (myservo2.read() > angle2) {
   //   for (pos=myservo2.read(); pos >= angle2; pos-=1) {
@@ -69,10 +148,10 @@ void loop() {
   //   }
   //   }
   // }
-  angle1 = q1;
-  angle2 = q2;
-  }
-}
+  // angle1 = q1;
+  // angle2 = q2;
+  // }
+//}
 // void movement(int angle, int angle2) {
 //   while (angle != 0) {
 //     // what is the current position fo the arm
